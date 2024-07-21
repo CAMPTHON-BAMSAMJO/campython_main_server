60f579cf70af25beaf42f1d8774171bca5be8a1a
Eunji <129590633+bianbbc87@users.noreply.github.com>
2024-07-21 23:59:06 +0900

    REFACTOR: createdAt 자동 할당용 매핑 추가

--- src/main/java/org/dongguk/camputhon/CamputhonApplication.java
+++ src/main/java/org/dongguk/camputhon/CamputhonApplication.java
@@ -2,9 +2,12 @@ package org.dongguk.camputhon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
-public class CamputhonApplication {
+public class
+CamputhonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CamputhonApplication.class, args);
