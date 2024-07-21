67b84174d2f24a11b4c3a5a674f9e42d8c0a0a08
Eunji <129590633+bianbbc87@users.noreply.github.com>
2024-07-21 23:51:51 +0900

    FEAT: swagger config 세팅

---
+++ src/main/java/org/dongguk/camputhon/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package org.dongguk.camputhon.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Camputhon")
+                        .description("BamSamJo")
+                        .version("1.0.0"));
+    }
+}
